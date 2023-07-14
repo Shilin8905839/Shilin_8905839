032397418e3ee60af0df6b340047896825125ad6
shilin <sdesai5839@conestogac.on.ca>
2023-07-13 20:41:03 -0400

    file added

--- Assignment_3/Assignment3.py
+++ Assignment_3/Assignment3.py
@@ -7,46 +7,34 @@ import time
 # Setting up the webdriver
 driver = webdriver.Chrome()
 
-# Navigating to the Google homepage
-driver.get("https://www.google.com")
+# Navigating to the YouTube homepage
+driver.get("https://www.youtube.com")
 time.sleep(3)
 
-# Finding the sign-in button and clicking on it
-sign_in_button = driver.find_element("xpath", "//a[contains(text(), 'Sign in')]")
-sign_in_button.click()
-
-# Waiting for the sign-in page to load
-time.sleep(5)
-
-# Entering the email/username
-email_input = driver.find_element("id", "identifierId")
-email_input.send_keys("your_email@example.com")
-email_input.send_keys(Keys.RETURN)
-
-# Waiting for the password page to load
-time.sleep(5)
-
-# Entering the password
-password_input = driver.find_element("name", "password")
-password_input.send_keys("your_password")
-password_input.send_keys(Keys.RETURN)
-
-# Waiting for the Google homepage to load after signing in
-time.sleep(5)
-
-# Verifying that the user is signed in
-assert "Google" in driver.title
-
-# Performing a search on Google
-search_bar = driver.find_element("name", "q")
-search_bar.send_keys("automation")
+# Searching for GitHub tutorials on YouTube
+search_bar = driver.find_element("name", "search_query")
+search_bar.send_keys("GitHub tutorial")
 search_bar.send_keys(Keys.RETURN)
 
 # Waiting for the search results page to load
 time.sleep(5)
 
 # Verifying that the search results page has loaded
-assert "automation" in driver.title
+assert "GitHub tutorial" in driver.title
+
+# Selecting the first video from the search results
+video_link = driver.find_element("xpath", "//a[@id='video-title']")
+video_link.click()
+
+# Waiting for the video page to load
+time.sleep(5)
+
+# Closing the video
+#close_button = driver.find_element("xpath", "//button[@aria-label='Close']")
+#close_button.click()
+
+# Waiting for the video to close
+time.sleep(3)
 
 # Closing the webdriver
 driver.close()
